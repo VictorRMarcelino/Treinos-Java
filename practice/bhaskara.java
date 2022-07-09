83933b1726bdd75c5cada5f9b21db7c7227d88d5
VictorRMarcelino <victorramosmarcelino@hotmail.com>
2022-07-08 22:35:19 -0300

    Update bhaskara

--- practice/bhaskara.java
+++ practice/bhaskara.java
@@ -3,7 +3,7 @@ package practice;
 import java.util.Scanner;
 
 public class bhaskara {
-    static int a, b, c, delta;
+    static double a, b, c, delta, invertedBValue, deltaRoot, value1, value2;
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -13,43 +13,65 @@ public class bhaskara {
     public static void EnterAValue(){
         try {
             System.out.println("enter the value of A: ");
-            a = scanner.nextInt();
+            a = scanner.nextDouble();
             EnterBValue();
         }catch (Exception e){
             System.out.println("The inserted element is invalid! " + e);
-        }finally {
-            EndOfProgram();
         }
     }
     public static void EnterBValue(){
         try {
             System.out.println("enter the value of B: ");
-            b = scanner.nextInt();
+            b = scanner.nextDouble();
             EnterCValue();
         }catch (Exception e){
             System.out.println("The inserted element is invalid! " + e);
-        }finally {
-            EndOfProgram();
         }
     }
     public static void EnterCValue(){
         try {
             System.out.println("enter the value of C: ");
-            c = scanner.nextInt();
+            c = scanner.nextDouble();
             calcDeltaValue();
         }catch (Exception e){
             System.out.println("The inserted element is invalid! " + e);
-        }finally {
-            EndOfProgram();
         }
     }
 
     public static void calcDeltaValue(){
         delta = (b * b) - (4 * a * c);
+        calcNegativeValueOfB();
     }
 
+    public static void calcNegativeValueOfB(){
+        invertedBValue = (b * - 1);
+        calcDeltaRoot();
+    }
 
-    public static void EndOfProgram(){
-        System.out.println("The program ends here!");
+    public static void calcDeltaRoot(){
+        deltaRoot = Math.sqrt(delta);
+        calcValue1();
+    }
+
+    public static void calcValue1(){
+        value1 = (invertedBValue + deltaRoot)/(2 * a);
+        calcValue2();
+    }
+
+    public static void calcValue2(){
+        value2 = (invertedBValue - deltaRoot)/(2 * a);
+        printResults();
+    }
+
+    public static void printResults(){
+        System.out.println("A: " + a);
+        System.out.println("B: " + b);
+        System.out.println("C: " + c);
+        System.out.println("Delta: " + delta);
+        System.out.println("Inverted of B Value: " + invertedBValue);
+        System.out.println("Delta root: " + deltaRoot);
+        System.out.println("");
+        System.out.println("Primeiro valor : " + value1);
+        System.out.println("Segundo valor : " + value2);
     }
 }
